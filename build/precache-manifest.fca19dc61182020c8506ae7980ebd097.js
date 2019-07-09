9fd5f0281b2e73b0d62aaa2c42f48ea810bc430e
Protik Sarkar <protik.sarkar@gmail.com>
2019-07-10 01:30:39 +0200

    final

---
+++ build/precache-manifest.fca19dc61182020c8506ae7980ebd097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61656a58f2fbe33f14a1e23acbc079de",
+    "url": "/Simple-React-TypeScript-ToDo/index.html"
+  },
+  {
+    "revision": "7a08fcc51bc74d846e15",
+    "url": "/Simple-React-TypeScript-ToDo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "31651e6a1d816aebc4aa",
+    "url": "/Simple-React-TypeScript-ToDo/static/js/2.f1d6255d.chunk.js"
+  },
+  {
+    "revision": "7a08fcc51bc74d846e15",
+    "url": "/Simple-React-TypeScript-ToDo/static/js/main.3aa102ad.chunk.js"
+  },
+  {
+    "revision": "359f6b1b9569b1fb0463",
+    "url": "/Simple-React-TypeScript-ToDo/static/js/runtime~main.4ca965a8.js"
+  },
+  {
+    "revision": "066543a624cf7d6679b6946ab2c4f7ef",
+    "url": "/Simple-React-TypeScript-ToDo/static/media/art.066543a6.svg"
+  }
+]);
